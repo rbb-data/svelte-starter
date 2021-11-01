8f21923cb9e6a6aaa30f9f00b1a7560fa73b4271
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-01 17:39:07 +0100

    Export app to static pages on build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,8 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
