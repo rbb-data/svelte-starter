9dd8c68ba1e516c270aed336b43f46f5c6ac67a7
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-08 20:40:13 +0100

    Format after style generations

--- src/style/variables.js
+++ src/style/variables.js
@@ -1,8 +1,8 @@
 /**
  * Do not edit directly
- * Generated on Mon, 08 Nov 2021 15:40:05 GMT
+ * Generated on Mon, 08 Nov 2021 19:39:27 GMT
  */
 
-export const breakpointXs = "320px";
-export const breakpointSm = "480px";
-export const breakpointMd = "768px";
+export const breakpointXs = '320px';
+export const breakpointSm = '480px';
+export const breakpointMd = '768px';
