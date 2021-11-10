5516cf670d2254af720516190610379c1e61f65e
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-10 15:05:43 +0100

    Add more viewports to storybook

--- doc/.storybook/preview.js
+++ doc/.storybook/preview.js
@@ -1,3 +1,5 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -5,5 +7,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
   },
 };
