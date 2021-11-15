c1b0d3325e299df382848d31b3d50e3147171aee
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-15 09:33:01 +0100

    Refactor

---
+++ src/lib/components/demo/Arrows.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { tweened } from 'svelte/motion';
+
+  import Arrow from './Arrow.svelte';
+  import css from '$lib/actions/css';
+  import { translate } from '$lib/helpers/utils';
+
+  export let orientations: Array<'n' | 'w' | 's' | 'e'> = ['n', 'w', 's', 'e'];
+  export let offset = 10;
+  export let show = true;
+
+  // position of each arrow
+  const translations = {
+    n: [0, -offset],
+    w: [offset, 0],
+    s: [0, offset],
+    e: [-offset, 0],
+  };
+
+  // toggle visibility of arrows via a smooth transition
+  let arrowOpacity = tweened(+show);
+  $: arrowOpacity.set(+show);
+</script>
+
+{#each orientations as orientation}
+  <g
+    class="arrow"
+    transform={translate(translations[orientation])}
+    use:css={{ opacity: $arrowOpacity }}
+  >
+    <Arrow {orientation} color="darkgrey" />
+  </g>
+{/each}
+
+<style>
+  .arrow {
+    opacity: var(--opacity);
+  }
+</style>
--- src/lib/components/demo/DraggableCircle.svelte
+++ src/lib/components/demo/DraggableCircle.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
   import { writable } from 'svelte/store';
-  import { tweened } from 'svelte/motion';
 
-  import Arrow from './Arrow.svelte';
-  import css from '$lib/actions/css';
+  import Arrows from './Arrows.svelte';
+
   import pannable, { handlePanMove } from '$lib/actions/pannable';
   import { translate } from '$lib/helpers/utils';
 
@@ -11,9 +10,10 @@
   export let cx: number;
   export let cy: number;
 
+  // size of the circle
   export let radius = 5;
 
-  // if given, dot is restricted to move within these bounds
+  // if given, the dot is restricted to move within these bounds
   export let bounds: {
     top: number;
     right: number;
@@ -21,40 +21,19 @@
     left: number;
   } = undefined;
 
-  // position of the doc (could also be a spring)
+  // position of the dot (could also be a spring)
   const coords = writable({ x: cx, y: cy });
 
-  // specify arrows
-  const offset = 4;
-  const arrows: Array<{
-    orientation: 'n' | 'w' | 's' | 'e';
-    xy: Array<number>;
-  }> = [
-    { orientation: 'n', xy: [0, -radius - offset] },
-    { orientation: 'w', xy: [radius + offset, 0] },
-    { orientation: 's', xy: [0, radius + offset] },
-    { orientation: 'e', xy: [-radius - offset, 0] },
-  ];
-
-  // arrows are hidden when a dot is dragged
-  let arrowOpacity = tweened(1);
+  let showArrows = true;
 </script>
 
 <g transform={translate([$coords.x, $coords.y])}>
-  {#each arrows as arrow}
-    <g
-      class="arrow"
-      transform={translate(arrow.xy)}
-      use:css={{ opacity: $arrowOpacity }}
-    >
-      <Arrow orientation={arrow.orientation} color="darkgrey" />
-    </g>
-  {/each}
+  <Arrows offset={radius + 4} show={showArrows} />
   <circle
     use:pannable
-    on:panstart={() => arrowOpacity.set(0)}
+    on:panstart={() => (showArrows = false)}
     on:panmove={handlePanMove(coords, { bounds })}
-    on:panend={() => arrowOpacity.set(1)}
+    on:panend={() => (showArrows = true)}
     r={radius}
   />
 </g>
@@ -66,9 +45,5 @@
 
   circle:hover {
     cursor: move;
-  }
-
-  .arrow {
-    opacity: var(--opacity);
   }
 </style>
