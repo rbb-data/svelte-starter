be1f1eefd30cd2a30c8d06baab0a9760f72bac3b
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-15 09:27:40 +0100

    Export actions to package

---
+++ src/lib/actions/index.ts
@@ -0,0 +1,8 @@
+/**
+ * These actions are exported as package.
+ */
+
+import css from './css';
+import pannable from './pannable';
+
+export { css, pannable };
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,3 @@
-/**
- * These components are exported as package.
- */
-
 import Svg from './components/shared/Svg.svelte';
 
 export { Svg };
