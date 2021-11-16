3344562d7f1b389eade71d270bc882ad21ae9b23
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-16 17:46:47 +0100

    Improve types

--- src/lib/svelte-jsx.d.ts
+++ src/lib/svelte-jsx.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 interface XY {
@@ -10,17 +11,16 @@ interface XYd extends XY {
   dy: number;
 }
 
+type Test = XYd;
+
+interface Pannable {
+  // custom events defined in `src/lib/actions/pannable.ts`
+  onpanstart?: (e: CustomEvent<XY>) => void;
+  onpanmove?: (e: CustomEvent<XYd>) => void;
+  onpanend?: (e: CustomEvent<XY>) => void;
+}
+
 declare namespace svelte.JSX {
-  interface SVGAttributes<T> {
-    // custom events defined in `src/lib/actions/pannable.ts`
-    onpanstart?: (e: CustomEvent<XY>) => void;
-    onpanmove?: (e: CustomEvent<XYd>) => void;
-    onpanend?: (e: CustomEvent<XY>) => void;
-  }
-  interface HTMLAttributes<T> {
-    // custom events defined in `src/lib/actions/pannable.ts`
-    onpanstart?: (e: CustomEvent<XY>) => void;
-    onpanmove?: (e: CustomEvent<XYd>) => void;
-    onpanend?: (e: CustomEvent<XY>) => void;
-  }
+  interface SVGAttributes<T> extends Pannable {}
+  interface HTMLAttributes<T> extends Pannable {}
 }
