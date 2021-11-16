ae57f50a9760c2f8f7bbd82f075b7da7df0599bc
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-16 13:15:31 +0100

    Swipe on mobile devices

--- src/lib/components/shared/Slider.svelte
+++ src/lib/components/shared/Slider.svelte
@@ -1,26 +1,45 @@
 <script lang="ts">
+  import { writable } from 'svelte/store';
   import { spring } from 'svelte/motion';
 
   import css from '$lib/actions/css';
+  import pannable, { drag } from '$lib/actions/pannable';
 
   export let onForwardNavigation: () => void;
   export let onBackwardNavigation: () => void;
 
   let width = 0;
   let height = 0;
+
   let animate = true;
+  let startX: number;
 
   $: minX = 0.33 * width;
   $: maxX = 0.66 * width;
+  const minDiff = 20;
 
   // TODO: this should only be x
-  let xy = { x: 0, y: 0 };
-  const animatedXY = spring(xy);
+  let xy = writable({ x: 0, y: 0 });
+  const animatedXY = spring({ x: 0, y: 0 });
 
-  $: animatedXY.set(xy);
+  $: animatedXY.set($xy);
 
-  function handleClick(event: MouseEvent) {
-    const { clientX: x } = event;
+  function navigateAndReset(navigateBackward: boolean) {
+    // transition to the next (or previous) slide
+    animate = true;
+    xy.set(navigateBackward ? { x: width, y: 0 } : { x: -width, y: 0 });
+
+    // reset after the transition is complete
+    setTimeout(() => {
+      animate = false;
+      xy.set({ x: 0, y: 0 });
+      if (navigateBackward) onBackwardNavigation();
+      else onForwardNavigation();
+    }, 500);
+  }
+
+  function handleClick(e: MouseEvent) {
+    const { clientX: x } = e;
 
     if (x >= minX && x <= maxX) return;
 
@@ -29,15 +48,24 @@
     if (navigateBackward && !$$slots.prev) return;
     if (!navigateBackward && !$$slots.next) return;
 
-    animate = true;
-    xy = navigateBackward ? { x: 100, y: 0 } : { x: -100, y: 0 };
+    navigateAndReset(navigateBackward);
+  }
 
-    setTimeout(() => {
-      animate = false;
-      xy = { x: 0, y: 0 };
-      if (navigateBackward) onBackwardNavigation();
-      else onForwardNavigation();
-    }, 500);
+  function handlePanEnd(e: CustomEvent<{ x: number; y: number }>) {
+    const endX = e.detail.x;
+    const diff = endX - startX;
+    const navigateBackward = diff > 0;
+
+    if (
+      Math.abs(diff) < minDiff ||
+      (navigateBackward && !$$slots.prev) ||
+      (!navigateBackward && !$$slots.next)
+    ) {
+      xy.set({ x: 0, y: 0 });
+      return;
+    }
+
+    navigateAndReset(navigateBackward);
   }
 </script>
 
@@ -49,13 +77,21 @@
   <div
     class="content"
     use:css={{
-      'translate-x': `${animate ? $animatedXY.x : xy.x}%`,
+      'translate-x': `${animate ? $animatedXY.x : $xy.x}px`,
     }}
   >
     <div class="inner prev">
       <slot name="prev" />
     </div>
-    <div class="inner" bind:clientHeight={height} on:click={handleClick}>
+    <div
+      class="inner"
+      bind:clientHeight={height}
+      on:click={handleClick}
+      use:pannable
+      on:panstart={(e) => (startX = e.detail.x)}
+      on:panmove={drag(xy)}
+      on:panend={handlePanEnd}
+    >
       <slot name="curr" />
     </div>
     <div class="inner next">
