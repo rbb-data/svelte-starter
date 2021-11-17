227be5a7387211a926e814ef1770e54804c5b2de
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-17 10:49:28 +0100

    Add basic tabs

---
+++ src/lib/components/shared/Tabs.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let tabs = [];
+  export let activeIndex = 0;
+</script>
+
+<ul class="tabs">
+  {#each tabs as tab, tabIndex}
+    <li
+      class:active={tabIndex === activeIndex}
+      on:pointerdown={() => (activeIndex = tabIndex)}
+    >
+      <svelte:component this={tab.component} {...tab.props} />
+    </li>
+  {/each}
+</ul>
+
+<style>
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+    display: flex;
+  }
+
+  li {
+    flex: 1;
+  }
+
+  li:hover {
+    cursor: pointer;
+  }
+</style>
