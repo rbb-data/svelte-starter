b1a65e4691ec029ba4caf2bf824a66550f42ec88
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-18 13:56:12 +0100

    Action on input element that fuzzy searches a query against data

---
+++ src/lib/actions/fuzzySearch.ts
@@ -0,0 +1,67 @@
+import fuzzysort from 'fuzzysort';
+
+import type { ActionReturn } from '$lib/types';
+
+interface SearchOptions {
+  data: Array<string | Record<string, any>>;
+  key?: string;
+  highlight?: boolean;
+}
+
+interface Suggestion {
+  item: string | Record<string, any>;
+  highlight?: string;
+}
+
+export default function fuzzySearch(
+  node: HTMLInputElement,
+  { data, key, highlight }: SearchOptions
+): ActionReturn<void> {
+  if (!highlight) highlight = false;
+
+  function search(query: string, top?: number): Array<Suggestion> {
+    let results = fuzzysort.go(query, data, { key });
+
+    if (top) results = results.slice(0, top) as any;
+
+    const suggestions = [];
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+      suggestions.push({
+        item: key ? result.obj : result.target,
+        highlight: highlight ? fuzzysort.highlight(result) : undefined,
+      });
+    }
+
+    return suggestions;
+  }
+
+  function handleInput(event: Event) {
+    const suggestions = search((event.target as HTMLInputElement).value);
+    node.dispatchEvent(
+      new CustomEvent('search', {
+        detail: { suggestions },
+      })
+    );
+  }
+
+  function handleChange(event: Event) {
+    const suggestions = search((event.target as HTMLInputElement).value, 1);
+    const result = suggestions.length > 0 ? suggestions[0] : null;
+    node.dispatchEvent(
+      new CustomEvent('result', {
+        detail: { result },
+      })
+    );
+  }
+
+  node.addEventListener('input', handleInput);
+  node.addEventListener('change', handleChange);
+
+  return {
+    destroy() {
+      node.removeEventListener('input', handleInput);
+      node.removeEventListener('change', handleChange);
+    },
+  };
+}
