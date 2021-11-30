33e6c9587a514f05d8ea533f6b9278d0f83004b6
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-11-30 09:59:00 +0100

    Simplify tabs pattern using slots

--- src/lib/components/shared/core/Tabs.svelte
+++ src/lib/components/shared/core/Tabs.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  import type { Component } from '$lib/types';
-
   import focus from '$lib/actions/focus';
 
-  // list of tabs (a tab is any svelte component that can be rendered)
-  export let tabs: Array<Component>;
+  // list of tabs
+  export let tabs: Array<string>;
 
   // initial index of the active tab
   export let initialIndex = 0;
@@ -32,9 +30,9 @@
       aria-selected={tabIndex === activeIndex}
       tabindex={tabIndex === activeIndex ? 0 : -1}
       on:pointerdown={() => (activeIndex = tabIndex)}
-      use:focus={activeIndex === tabIndex}
+      use:focus={tabIndex === activeIndex}
     >
-      <svelte:component this={tab.component} {...tab.props} />
+      <slot {tab} active={tabIndex === activeIndex} />
     </li>
   {/each}
 </ul>
