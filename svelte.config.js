e14c086a66833fe731c8b654e11eac82e7b46575
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-12-03 13:34:49 +0100

    Remove ts-related packages and setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 import dotenv from 'dotenv';
@@ -9,10 +8,6 @@ const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
-
   kit: {
     adapter: adapter({ pages: process.env.BUILD_DIR }),
     paths: {
