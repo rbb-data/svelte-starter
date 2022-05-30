7546ec4e785aad22d0612329d685d37926755c36
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-05-30 18:05:54 +0200

    simplify demo; demonstrate how to load data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,149 @@
 <script>
-  import Demo from '$lib/components/demo/Demo.svelte';
-
   import googleDoc from '$data/google-doc.json';
-  import data from '$data/example-data.csv';
   import googleSheetData from '$data/google-sheet-example.csv';
-
-  const { header } = googleDoc;
+  import localData from '$data/example-data.csv';
 </script>
 
 <div class="wrapper">
-  <Demo {data} {header} />
-  <pre>{JSON.stringify(googleSheetData, null, 2)}</pre>
+  <h1>{googleDoc.title}</h1>
+
+  <p>
+    Dieser Code lädt beispielhaft lokale Daten und ist zudem mit einem
+    <a
+      href="https://docs.google.com/document/d/1wCovwTGxPsPM-ED-D7hCaL5sMUFBy1A8OadVUCDtQ3A"
+      target="_blank"
+    >
+      Google doc
+    </a>
+    und einem
+    <a
+      href="https://docs.google.com/spreadsheets/d/1RPOs51w4kJsvuNg1eT0foVgLau_iI7hmJ-EOGQqBC04"
+      target="_blank"
+    >
+      Google sheet
+    </a>
+    verbunden.
+  </p>
+
+  <h2>Lokale Daten</h2>
+
+  <p>
+    Die lokalen Daten liegen in <code>src/data/example-data.csv</code>
+    und werden als <code>localData</code> importiert:
+  </p>
+
+  <table>
+    <thead>
+      <tr>
+        <th>x</th>
+        <th>y</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each localData as row, i}
+        <tr>
+          <td>{row.x}</td>
+          <td>{row.y}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+
+  <h2>Google Sheet</h2>
+
+  <p>
+    Das Google sheet liegt in <code>src/data/google-sheet-example.csv</code>
+    und wird als <code>googleSheetData</code> importiert:
+  </p>
+
+  <table>
+    <thead>
+      <tr>
+        <th>col1</th>
+        <th>col2</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each googleSheetData as row, i}
+        <tr>
+          <td>{row.col1}</td>
+          <td>{row.col2}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+
+  <h2>Google doc</h2>
+
+  <p>
+    Das Google doc liegt in <code>src/data/google-doc.json</code>
+    und wird als <code>googleDoc</code> importiert. Ein paar Beispiele:
+  </p>
+
+  <h3>Einfaches Objekt</h3>
+  {googleDoc.person.name} ({googleDoc.person.age})
+
+  <h3>Liste von Strings</h3>
+  <ul>
+    {#each googleDoc.listOfStrings as textBlock}
+      <li>{@html textBlock}</li>
+    {/each}
+  </ul>
+
+  <h3>Liste von Objekten</h3>
+  <ol>
+    {#each googleDoc.arrayOfObjects as person}
+      <li>{person.name} ({person.age})</li>
+    {/each}
+  </ol>
 </div>
 
-<style>
+<style lang="scss">
   .wrapper {
     max-width: 800px;
     margin: 0 auto;
+  }
+
+  h1 {
+    font-size: calc(1.5 * var(--font-size-base));
+  }
+
+  h2 {
+    font-size: calc(1.25 * var(--font-size-base));
+    margin-top: var(--s-rem-6);
+  }
+
+  h3 {
+    font-size: var(--font-size-base);
+    margin-top: var(--s-rem-3);
+  }
+
+  h1,
+  h2,
+  h3 {
+    margin-bottom: var(--s-rem-1);
+  }
+
+  p {
+    margin-bottom: var(--s-rem-1);
+  }
+
+  table {
+    font-family: monospace;
+    margin: var(--s-rem-2) 0;
+    border: 1px solid;
+
+    th,
+    td {
+      border: 1px solid;
+    }
+
+    thead th {
+      text-align: left;
+    }
+
+    tbody td {
+      text-align: right;
+    }
   }
 </style>
