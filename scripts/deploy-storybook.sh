329d22245fe8d5c9c7ab07d03562aaa4f706a105
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-06-14 17:58:37 +0200

    fix: add bot identity

--- scripts/deploy-storybook.sh
+++ scripts/deploy-storybook.sh
@@ -13,6 +13,8 @@ git add -A
 git commit -m 'deploy storybook'
 
 # push to gh-pages branch
+git config --global user.email "deploy-bot@example.com"
+git config --global user.name "deploy-bot"
 git push -f git@github.com:rbb-data/svelte-starter.git main:gh-pages
 
 cd -
