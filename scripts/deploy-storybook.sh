332eaf56528873271bb16d0e9ca31f701b7353ab
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-06-14 18:03:48 +0200

    fix

--- scripts/deploy-storybook.sh
+++ scripts/deploy-storybook.sh
@@ -9,12 +9,13 @@ npm run storybook:build
 cd docs-build
 
 git init
+git config --global user.email "deploy-bot@example.com"
+git config --global user.name "deploy-bot"
+
 git add -A
 git commit -m 'deploy storybook'
 
 # push to gh-pages branch
-git config --global user.email "deploy-bot@example.com"
-git config --global user.name "deploy-bot"
 git push -f git@github.com:rbb-data/svelte-starter.git main:gh-pages
 
 cd -
