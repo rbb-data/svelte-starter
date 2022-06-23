7de65e57e117e85ab3ed7ffb090fe1ffe19f6ad2
sophiamersmann <sophia.mersmann1@gmail.com>
2022-06-23 10:12:33 +0200

    add bezier arrows

---
+++ src/lib/components/shared/BezierArrow.stories.svelte
@@ -0,0 +1,106 @@
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+  import Svg from './Svg.svelte';
+  import BezierArrow from './BezierArrow.svelte';
+</script>
+
+<Meta
+  title="Data Viz/BezierArrow"
+  component={BezierArrow}
+  argTypes={{
+    start: {
+      // @ts-ignore
+      type: { required: true },
+    },
+    end: {
+      // @ts-ignore
+      type: { required: true },
+    },
+    startHandleOffset: {
+      table: {
+        defaultValue: { summary: '[0, 0]' },
+      },
+    },
+    endHandleOffset: {
+      table: {
+        defaultValue: { summary: '[0, 0]' },
+      },
+    },
+  }}
+/>
+
+<Story name="Basic">
+  <Svg width={100} height={100}>
+    <BezierArrow start={[0, 0]} end={[100, 100]} />
+  </Svg>
+</Story>
+
+<Story name="Debug">
+  <Svg width={100} height={100}>
+    <BezierArrow start={[0, 0]} end={[100, 100]} debug />
+  </Svg>
+</Story>
+
+<Story name="Curve (specified by offsetting start and end points)">
+  <Svg width={100} height={100}>
+    <BezierArrow
+      start={[0, 0]}
+      end={[100, 100]}
+      startHandleOffset={[40, 0]}
+      endHandleOffset={[0, -40]}
+      debug
+    />
+  </Svg>
+</Story>
+
+<Story name="Curve (start and end handle manually specified)">
+  <Svg width={100} height={100}>
+    <BezierArrow
+      start={[0, 0]}
+      end={[100, 100]}
+      startHandle={[40, 0]}
+      endHandle={[100, 60]}
+      debug
+    />
+  </Svg>
+</Story>
+
+<Story name="Arrow heads on both sides">
+  <Svg width={100} height={100}>
+    <BezierArrow
+      start={[0, 0]}
+      end={[100, 100]}
+      startHandle={[40, 0]}
+      endHandle={[100, 60]}
+      headAnchor="both"
+      debug
+    />
+  </Svg>
+</Story>
+
+<Story name="Narrow arrow head">
+  <Svg width={100} height={100}>
+    <BezierArrow
+      start={[0, 0]}
+      end={[100, 100]}
+      startHandle={[40, 0]}
+      endHandle={[100, 60]}
+      headAngle={30}
+      debug
+    />
+  </Svg>
+</Story>
+
+<Story name="Large arrow head">
+  <Svg width={100} height={100}>
+    <BezierArrow
+      start={[0, 0]}
+      end={[100, 100]}
+      startHandle={[40, 0]}
+      endHandle={[100, 60]}
+      headLength={12}
+      debug
+    />
+  </Svg>
+</Story>
---
+++ src/lib/components/shared/BezierArrow.svelte
@@ -0,0 +1,198 @@
+<script>
+  import { cUiGray500 } from '$lib/tokens';
+
+  /**
+   * start coordinates
+   *
+   * @type {number[]}
+   */
+  export let start;
+
+  /**
+   * end coordinates
+   *
+   * @type {number[]}
+   */
+  export let end;
+
+  /**
+   * bezier handle offset from start coordinates
+   *
+   * @type {number[]}
+   */
+  export let startHandleOffset = [0, 0];
+
+  /**
+   * bezier handle offset from end coordinates
+   *
+   * @type {number[]}
+   */
+  export let endHandleOffset = [0, 0];
+
+  /**
+   * start bezier handle coordinates; if given, `startHandleOffset` is ignored
+   *
+   * @type {number[]}
+   */
+  export let startHandle = undefined;
+
+  /**
+   * end bezier handle coordinates; if given, `endHandleOffset` is ignored
+   *
+   * @type {number[]}
+   */
+  export let endHandle = undefined;
+
+  /**
+   * position of the arrow head
+   *
+   * @type {'start' | 'end' | 'both'}
+   */
+  export let headAnchor = 'end';
+
+  /**
+   * length of the arrow head
+   *
+   * @type {number}
+   */
+  export let headLength = undefined;
+
+  /** angle of the arrow head */
+  export let headAngle = 55;
+
+  /** @type {string} */
+  export let color = cUiGray500;
+
+  /** renders bezier handle points for debugging */
+  export let debug = false;
+
+  /**
+   * @param {number[]} p1
+   * @param {number[]} p2
+   */
+  const dist = (p1, p2) =>
+    Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2));
+
+  /**
+   * @param {number} n
+   * @param {number} min
+   * @param {number} max
+   */
+  const clamp = (n, min = 0, max = 1) => Math.min(Math.max(n, min), max);
+
+  /**
+   * @param {number[]} p1
+   * @param {number[]} p2
+   */
+  const equals = (p1, p2) => p1[0] === p2[0] && p1[1] === p2[1];
+
+  /**
+   * @param {number[]} p
+   * @param {number[]} o
+   */
+  const addOffset = (p, o) => [p[0] + o[0], p[1] + o[1]];
+
+  /**
+   * @param {number[]} start
+   * @param {number[]} end
+   * @param {number[]} startHandle
+   * @param {number[]} endHandle
+   */
+  const bezierCurve = (start, end, startHandle, endHandle) =>
+    ['M', `${start}`, 'C', `${startHandle}`, `${endHandle}`, `${end}`].join(
+      ' '
+    );
+
+  /**
+   * @param {number[]} point
+   * @param {number[]} handle
+   */
+  function arrowHead(point, handle, { length = 4, theta = 45 } = {}) {
+    const xLen = handle[0] - point[0];
+    const yLen = handle[1] - point[1];
+
+    const distance = Math.sqrt(Math.pow(xLen, 2) + Math.pow(yLen, 2));
+    const ratio = length / distance;
+
+    const mid = [point[0] + xLen * ratio, point[1] + yLen * ratio];
+
+    /**
+     * @param {number[]} p
+     * @param {number[]} pivot
+     * @param {number} theta
+     */
+    function rotate(p, pivot, theta) {
+      const thetaRad = (theta * Math.PI) / 180;
+      return [
+        pivot[0] +
+          (p[0] - pivot[0]) * Math.cos(thetaRad) -
+          (p[1] - pivot[1]) * Math.sin(thetaRad),
+        pivot[1] +
+          (p[0] - pivot[0]) * Math.sin(thetaRad) +
+          (p[1] - pivot[1]) * Math.cos(thetaRad),
+      ];
+    }
+
+    return [
+      'M',
+      rotate(mid, point, theta),
+      'L',
+      point,
+      'L',
+      rotate(mid, point, -theta),
+    ].join(' ');
+  }
+
+  $: sHandle = startHandle || addOffset(start, startHandleOffset);
+  $: eHandle = endHandle || addOffset(end, endHandleOffset);
+
+  $: headOptions = {
+    length: headLength || clamp(0.08 * dist(start, end), 4, 8),
+    theta: headAngle,
+  };
+</script>
+
+<g class="arrow" style:--color={color}>
+  {#if debug}
+    <g class="debug">
+      {#each [sHandle, eHandle] as coords}
+        <circle cx={coords[0]} cy={coords[1]} r="5" />
+      {/each}
+      {#each [[start, sHandle], [end, eHandle]] as [s, e]}
+        <line x1={s[0]} y1={s[1]} x2={e[0]} y2={e[1]} />
+      {/each}
+    </g>
+  {/if}
+
+  <path d={bezierCurve(start, end, sHandle, eHandle)} />
+  {#if headAnchor === 'start' || headAnchor === 'both'}
+    <path
+      d={arrowHead(start, equals(start, sHandle) ? end : sHandle, headOptions)}
+    />
+  {/if}
+  {#if headAnchor === 'end' || headAnchor === 'both'}
+    <path
+      d={arrowHead(end, equals(end, eHandle) ? start : eHandle, headOptions)}
+    />
+  {/if}
+</g>
+
+<style lang="scss">
+  path {
+    stroke: var(--color);
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    fill: none;
+  }
+
+  .debug {
+    circle {
+      fill: none;
+      stroke: orange;
+    }
+
+    line {
+      stroke: orange;
+    }
+  }
+</style>
