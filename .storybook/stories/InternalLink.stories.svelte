894fb2c3c3f8cc8c9a46565f70bb97851052320f
sophiamersmann <sophia.mersmann1@gmail.com>
2022-10-25 14:47:44 +0200

    fix storybook

--- .storybook/stories/InternalLink.stories.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import { Meta } from '@storybook/addon-svelte-csf';
-
-  import InternalLink from '$comps/shared/InternalLink.svelte';
-</script>
-
-<Meta title="UI/InternalLink" component={InternalLink} />
