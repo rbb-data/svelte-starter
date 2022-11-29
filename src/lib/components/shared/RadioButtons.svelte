bafb7032a6fc40a2cc1b378b80d01e466a0f1346
sophiamersmann <sophia.mersmann1@gmail.com>
2022-11-29 16:26:48 +0100

    simplify radio buttons

--- src/lib/components/shared/RadioButtons.svelte
+++ src/lib/components/shared/RadioButtons.svelte
@@ -2,11 +2,16 @@
   /**
    * Group of vertically aligned radio buttons.
    *
+   * CSS variables:
+   *
+   * - `--ui-color-accent`: accent color _(default: blue)_
+   * - `--ui-color-secondary`: secondary color _(default: light blue)_
+   *
    * The rendered markup is composed of:
    *
    * - `.radio-buttons`: assigned the given id
    * - `.radio-buttons legend`
-   * - `.radio-buttons .radio-button`: with classes `.focused`, `.checked` and
+   * - `.radio-buttons .radio-button`: with classes `.focused`, `.selected` and
    *   `.disabled` applied appropriately
    * - `.radio-buttons .radio-button input[type="radio"]`
    *
@@ -14,9 +19,6 @@
    *
    * @component
    */
-
-  import * as tokens from '$lib/tokens';
-  import { cAccentId, c100Id } from '$lib/utils';
 
   /**
    * globally unique id
@@ -33,44 +35,21 @@
   export let options;
 
   /**
+   * label of the input group
+   *
+   * @type {string}
+   */
+  export let label;
+
+  /**
    * currently selected value
    *
    * @type {any}
    */
-  export let selectedValue;
-
-  /**
-   * label of the input group
-   *
-   * **note:** if not provided, `aria-labelledby` or `aria-label` must be used instead
-   *
-   * @type {string | undefined}
-   */
-  export let label = undefined;
+  export let selectedOption;
 
   /** hides label visually but keep it around for screen readers */
   export let hideLabelVisually = false;
-
-  /**
-   * sets CSS variables `--c-accent`, `--c-light` and `--c-focus` to pre-defined colors
-   *
-   * @type {Exclude<import('$lib/types').AccentColor, 'black'>}
-   */
-  export let colorScheme = 'blue';
-
-  /**
-   * sets CSS variables `--c-accent`, `--c-light` and `--c-focus`
-   *
-   * @type {{ accent?: string; light?: string; focus?: string }}
-   */
-  export let customColors = {};
-
-  /**
-   * function that maps an option to its value
-   *
-   * @type {(option: any) => any}
-   */
-  export let getOptionValue = (option) => option;
 
   /**
    * function that maps an option to `true` if disabled
@@ -79,68 +58,59 @@
    */
   export let isOptionDisabled = () => false;
 
-  /**
-   * ARIA attributes
-   *
-   * @type {{
-   *   label?: string;
-   *   labelledby?: string;
-   *   describedby?: string;
-   *   orientation?: 'horizontal' | 'vertical';
-   * }}
-   */
-  export let aria = { orientation: 'vertical' };
-
-  $: color = customColors.accent || tokens[cAccentId(colorScheme)];
-  $: colorLight = customColors.light || tokens[c100Id(colorScheme)];
-  $: colorFocus = customColors.focus || color;
+  // $: color = customColors.accent || tokens[cAccentId(colorScheme)];
+  // $: colorLight = customColors.light || tokens[c100Id(colorScheme)];
+  // $: colorFocus = customColors.focus || color;
 
   /** @type {any} */
-  let focusedValue = null;
+  let focusedOption = null;
 </script>
 
-<fieldset
-  {id}
-  class="radio-buttons"
-  style:--c-accent={color}
-  style:--c-light={colorLight}
-  style:--c-focus={colorFocus}
-  aria-label={aria.label}
-  aria-labelledby={aria.labelledby}
-  aria-describedby={aria.describedby}
->
+<fieldset {id} class:radio-buttons={true} class={$$restProps.class}>
   {#if label}
     <legend class:visually-hidden={hideLabelVisually}>
       {label}
     </legend>
   {/if}
   {#each options as option}
-    {@const v = getOptionValue(option)}
-    {@const checked = v === selectedValue}
+    {@const checked = option === selectedOption}
     {@const disabled = isOptionDisabled(option)}
-    {@const focused = v === focusedValue}
-    <label class="radio-button" class:focused class:checked class:disabled>
+    {@const focused = option === focusedOption}
+    <label
+      class="radio-button"
+      class:focused
+      class:checked
+      class:selected={checked}
+      class:disabled
+    >
       <input
         type="radio"
         name={id}
-        value={v}
+        value={option}
         {disabled}
-        bind:group={selectedValue}
+        bind:group={selectedOption}
         on:focus={(e) => {
           const node = e.currentTarget;
           if (node.classList.contains('focus-visible')) {
-            focusedValue = v;
+            focusedOption = option;
           }
         }}
-        on:blur={() => (focusedValue = null)}
+        on:blur={() => (focusedOption = null)}
       />
-      <slot {option} {checked} />
+      <slot {option} selected={checked}>
+        {option}
+      </slot>
     </label>
   {/each}
 </fieldset>
 
 <style lang="scss">
   fieldset {
+    --_ui-color-accent: var(--ui-color-accent, var(--c-ui-accent-blue));
+    --_ui-color-secondary: var(--ui-color-secondary, var(--c-blue-100));
+
+    --c-focus: var(--_ui-color-accent);
+
     width: 100%;
     border: 0;
     margin: 0;
@@ -155,9 +125,8 @@
 
   label {
     display: block;
-    cursor: pointer;
     padding: var(--s-px-2);
-    background-color: var(--c-light);
+    background-color: var(--_ui-color-secondary);
     margin: var(--s-px-2) 0;
     font-size: var(--font-size-xs);
     font-weight: var(--font-weight-semi-bold);
@@ -171,7 +140,6 @@
     }
 
     &.disabled {
-      cursor: default;
       opacity: 0.3;
     }
   }
@@ -190,7 +158,7 @@
     }
 
     &:checked {
-      border: 5px solid var(--c-accent);
+      border: 5px solid var(--_ui-color-accent);
       background-color: white;
     }
   }
